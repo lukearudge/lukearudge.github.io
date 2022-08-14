7050b64d1492c1ed066177cbc4a88db123d77d05
Luke A. Rudge <70853595+lukearudge@users.noreply.github.com>
2022-08-14 15:38:30 +0100

    Accordion changes and Summer Update Pt 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -398,4 +398,21 @@
 						$main._show(location.hash.substr(1), true);
 					});
 
+
+		// Accordion.
+
+		var acc = document.getElementsByClassName("accordion");
+		var i;
+		
+		for (i = 0; i < acc.length; i++) {
+		  acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+			  panel.style.maxHeight = null;
+			} else {
+			  panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		  });
+		}
 })(jQuery);
